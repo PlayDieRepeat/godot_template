4578b43a1f29d12eca20b2bd091ff376cf33109e
Richard Ortiz <SlowFaller@users.noreply.github.com>
2024-05-25 21:55:23 -0700

    Fixed a typo and some Windows whitespace shenanegins.

--- scripts/_autoloads/audio_bank_robber.gd
+++ scripts/_autoloads/audio_bank_robber.gd
@@ -3,7 +3,7 @@ extends Node
 var has_loaded:= false
 
 func _process(_p_delta) -> void:
-    if has_loaded:
-        return
+	if has_loaded:
+		return
 
-    has_loaded = true
+	has_loaded = true
--- scripts/systems/initializer.gd
+++ scripts/systems/initializer.gd
@@ -2,14 +2,14 @@ extends Node
 class_name Initializer
 
 @export var scene_override: RScene
-@export var use_ovveride: bool
+@export var use_overide: bool
 
 var ready_to_start_game:= false
 
 func _process(delta: float) -> void:
-    if ready_to_start_game == true:
-        return
-    
-    if (SoundManager.has_loaded == true) and (MusicManager.has_loaded == true) and (AudioBankRobber.has_loaded == true):
-        ready_to_start_game = true
-        Scenester.load_initial_scene()
+	if ready_to_start_game == true:
+		return
+	
+	if (SoundManager.has_loaded == true) and (MusicManager.has_loaded == true) and (AudioBankRobber.has_loaded == true):
+		ready_to_start_game = true
+		Scenester.load_initial_scene()
